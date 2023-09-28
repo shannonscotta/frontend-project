dd3b595ea130dc050430618b1b99ab6c915861ed
Scott Shannon <scott.a.shannon@outlook.com>
2023-09-27 21:18:45 -0700

    have week data, need to filter out descriptions and get mins and max from each days temp arr

--- README.md
+++ README.md
@@ -1,12 +1,13 @@
 # frontend-project
-16 hour frontend project using event listeners, AJAX, and DOM manipulation
+
+frontend project using event listeners, AJAX, and DOM manipulation
 
 ## Weather App
 
 - design credit @ https://uizard.io/templates/web-app-templates/weather-web-app-light/
 
 - created config.js to store api key
-- used . gitignore to hide config.js
+- used . gitignore to "hide" config.js
 
 !["lightmode design"](weather-app-lm.png)
 !["darkmode design](weather-app-dm.png)
@@ -16,3 +17,5 @@
 - whatthefont
 - config.js and .ignore for hidden key (until prod)
 - SVG's from https://www.svgrepo.com/
+- use https://flexboxfroggy.com/ religiously, skip through to go to the one you're trying to apply
+- change background color or add border color when tweaking styles.
--- main.js
+++ main.js
@@ -6,11 +6,6 @@ const form = document.getElementById("city-input-container");
 const mainTemp = document.querySelector(".main-temperature");
 const mainImage = document.querySelector(".main-image");
 
-const acTopLeft = document.querySelector(".ac-top-left");
-const acTopRight = document.querySelector(".ac-top-right");
-const acBottomLeft = document.querySelector(".ac-bottom-left");
-const acBottomRight = document.querySelector(".ac-bottom-right");
-
 const tfContainer = document.querySelector(".tf-item-container");
 
 let api = `https://api.openweathermap.org/data/2.5/forecast`;
@@ -37,48 +32,32 @@ function requestWeather(api, city, KEY, units) {
   $.get({
     url: `${api}?q=${city}&appid=${KEY}&${units}`,
     success: (data) => {
-      // get weather data in a good format
-
       /* -----------------------------------------Create weather array from JSON data-------------------------------------------------------------*/
-      let weatherArr = [];
+      let allWeatherArr = [];
 
       for (let timeObj of data.list) {
-        let weatherObj = {};
+        // console.log('timeObj', timeObj);
+        let allWeatherObj = {};
 
+        let day = timeObj.dt_txt;
         let date = timeObj.dt_txt.slice(5, 10);
         let time = timeObj.dt_txt.slice(11, 16);
         let description = timeObj.weather[0].description;
         let temp = timeObj.main["temp"];
+        let tempMin = timeObj.main["temp_min"];
+        let tempMax = timeObj.main["temp_max"];
 
-        weatherObj["date"] = date;
-        weatherObj["time"] = time;
-        weatherObj["description"] = description;
-        weatherObj["temp"] = `${celsiusToFahrenheit(temp)}°`;
+        allWeatherObj["date"] = date;
+        allWeatherObj["time"] = time;
+        allWeatherObj["description"] = description;
+        allWeatherObj["temp"] = celsiusToFahrenheit(temp);
+        allWeatherObj["minTemp"] = celsiusToFahrenheit(tempMin);
+        allWeatherObj["maxTemp"] = celsiusToFahrenheit(tempMax);
+        allWeatherObj["day"] = getDayOfWeek(day);
 
-        weatherArr.push(weatherObj);
+        allWeatherArr.push(allWeatherObj);
       }
-
-      /* ----------------------------------------- Append todays forecast items-------------------------------------------------------------*/
-
-      // remove all tf items before appending new ones
-      removeAllChildren(tfContainer);
-
-      //grab the next 6 entries of weather data
-      for (let i = 0; i < 6; i++) {
-        let tfObj = weatherArr[i];
-
-        let tfItem = document.createElement("div");
-        tfItem.classList = "tf-item";
-
-        //add time, image, and temp to tf item
-        tfItem.innerHTML = `${militaryToNormalTime(
-          tfObj.time
-        )} <img src="${getWeatherImage(
-          tfObj.description
-        )}" alt="weather icon" class="tf-item-image"/> <p>${tfObj.temp}</p>`;
-
-        tfContainer.appendChild(tfItem);
-      }
+      // console.log(allWeatherArr)
 
       /* -----------------------------------------Main weather data-------------------------------------------------------------*/
       //main weather: city
@@ -97,15 +76,33 @@ function requestWeather(api, city, KEY, units) {
         mainImage.src = getWeatherImage(currentDescription);
       }
 
+      /* ----------------------------------------- Append todays forecast items-------------------------------------------------------------*/
+
+      // remove all tf items before appending new ones
+      removeAllChildren(tfContainer);
+
+      //grab the next 6 entries of weather data
+      for (let i = 0; i < 6; i++) {
+        let tfObj = allWeatherArr[i];
+
+        let tfItem = document.createElement("div");
+        tfItem.classList = "tf-item";
+
+        //add time, image, and temp to tf item
+        tfItem.innerHTML = `${militaryToNormalTime(
+          tfObj.time
+        )} <img src="${getWeatherImage(
+          tfObj.description
+        )}" alt="weather icon" class="tf-item-image"/> <p>${tfObj.temp}°</p>`;
+
+        tfContainer.appendChild(tfItem);
+      }
+
       /* -----------------------------------------Air conditions card-------------------------------------------------------------*/
 
-      //Air Conditions: chance of rain
-      let chanceOfRain = data.list[0].pop;
-      acBottomLeft.innerHTML = `<img src="icons/rain-drop.svg" alt="rain icon"/> Chance of rain<p>${
-        chanceOfRain * 100
-      }%</p>`;
-
       //Air Conditions: feels like
+      const acTopLeft = document.querySelector(".ac-top-left");
+
       let feelsLike = data.list[0].main["feels_like"];
       if (feelsLike > 0) {
         acTopLeft.innerHTML = `<img src="icons/thermostat.svg" alt="thermostat icon"/> Real feel <p>${celsiusToFahrenheit(
@@ -114,14 +111,88 @@ function requestWeather(api, city, KEY, units) {
       }
 
       //Air Conditions: wind
+      const acTopRight = document.querySelector(".ac-top-right");
+
       let windSpeed = data.list[0].wind["speed"];
       acTopRight.innerHTML = `<img src="icons/wind.svg" alt="wind icon"/> Wind <p>${convertKPHtoMPH(
         windSpeed
       )} mph </p>`;
 
+      //Air Conditions: chance of rain
+      const acBottomLeft = document.querySelector(".ac-bottom-left");
+
+      let chanceOfRain = data.list[0].pop;
+      acBottomLeft.innerHTML = `<img src="icons/rain-drop.svg" alt="rain icon"/> Chance of rain<p>${
+        chanceOfRain * 100
+      }%</p>`;
+
       //Air Conditions: humidity
+      const acBottomRight = document.querySelector(".ac-bottom-right");
+
       let humidity = data.list[0].main["humidity"];
       acBottomRight.innerHTML = `<img src="icons/humidity.svg" alt="humidity icon"/> Humidity <p>${humidity}%</p>`;
+
+      /* -----------------------------------------Weekly forecast card-------------------------------------------------------------*/
+
+      // get tf data
+
+      let weekObj = {
+        Monday: { temp: [], description: [] },
+        Tuesday: { temp: [], description: [] },
+        Wednesday: { temp: [], description: [] },
+        Thursday: { temp: [], description: [] },
+        Friday: { temp: [], description: [] },
+        Saturday: { temp: [], description: [] },
+        Sunday: { temp: [], description: [] },
+      };
+
+      for (let i = 0; i < allWeatherArr.length; i++) {
+        let increment = allWeatherArr[i];
+
+        let day = increment.day;
+        let minTemp = increment.minTemp;
+        let maxTemp = increment.maxTemp;
+        let description = increment.description;
+
+        weekObj[day].temp.push(minTemp, maxTemp);
+        weekObj[day].description.push(description);
+      }
+
+
+      //TODO:
+      // sort each weekObj.day temp and then save the min and max for later
+      // filter out filler words in description and then count each word occurance, what appears the most? save that for the day
+      console.log(weekObj);
+
+
+
+      // TODO://
+
+      // find out what day it is
+      // let today = new Date();
+      //will need to call getDayOfWeek(today) to figure out which day to start painting next
+
+
+
+      // grab wf container
+      let wfContainer = document.querySelector(".week-forecast-container");
+
+      for (let i = 0; i <= 6; i++) {
+        //create wf item container
+        let wfItem = document.createElement("div");
+        wfItem.classList = "wf-item";
+
+        wfItem.innerHTML = `<div class="wf-day">Today</div>
+  
+        <div class="wf-image-container"> 
+  
+        <img class="wf-item-image" src="images/cloud.png">&nbsp&nbsp Sunny
+        </div>
+  
+        <div class="wf-temp">36 / 22</div>`;
+
+        wfContainer.appendChild(wfItem);
+      }
     },
   });
 }
@@ -176,7 +247,7 @@ function convertKPHtoMPH(kph) {
 }
 
 function militaryToNormalTime(militaryTime) {
-  // Split the military time into hours
+  // Split the military time into hours, second param is for the radix. 10 means decimal
   const hours = parseInt(militaryTime, 10);
   const period = hours < 12 ? "AM" : "PM";
 
@@ -196,3 +267,48 @@ function removeAllChildren(parent) {
     parent.removeChild(parent.firstChild);
   }
 }
+
+function getDayOfWeek(dateString) {
+  // parse into date object
+  let date = new Date(dateString);
+
+  //get day as num val 0 - 6
+  let dayOfWeekIndex = date.getDay();
+
+  // create arr to pass in num val as index
+  let daysOfWeek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  return daysOfWeek[dayOfWeekIndex];
+}
+
+
+//TODO:// autocomplete for searching city!
+
+//TODO:// center the air condition icon and word
+
+      //TODO:// implement geolocation?
+      // if (navigator.geolocation) {
+      //   navigator.geolocation.getCurrentPosition(function(position) {
+      //     const latitude = position.coords.latitude;
+      //     const longitude = position.coords.longitude;
+      //     console.log(`Latitude: ${latitude}, Longitude: ${longitude}`);
+      //   });
+      // } else {
+      //   console.log("Geolocation is not supported by this browser.");
+      // }
+
+      //TODO:// Reverse geocoding to get city name by lat and long
+
+      // 7 day api?
+      //
+
+
+      //TODO:// styling for air conditions
